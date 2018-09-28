15f0a1991102341614111506efc05798a05d26a7
Nicolas Qsdfsdgdhfgjs <nicolas.audepin@gmail.com>
2018-09-28 14:04:52 +0200

    Création de la classe GUIManager
    GUIM est totalement vide actuellement
    il faut maintenant déplacer toutes les fonctions une par unes

---
+++ src/manager/GUIManager.java
@@ -0,0 +1,19 @@
+package manager;
+
+
+/**
+ * Le GUI Manager est la classe qui va s'occuper de faire la liaison entre les classes du GUI et les classes de calcules
+ * toutes les classes du GUI feront appels à lui pour les interaction avec les filtres ou les les images.
+ * @author Nicolas2
+ *
+ */
+public class GUIManager { 
+	
+	
+	public GUIManager() {
+		
+	}
+	
+	
+
+}
